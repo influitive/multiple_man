9268d5de9c56c15dd45d0666e96417e7b6202adb
Ryan Brunner <ryan@influitive.com>
2013-12-03 07:16:29 -0500

    Creating, updating and destroying

--- lib/multiple_man/model_subscriber.rb
+++ lib/multiple_man/model_subscriber.rb
@@ -6,8 +6,38 @@ module MultipleMan
       attr_accessor :subscriptions
 
       def register(klass)
-        self.subscriptions << klass
+        self.subscriptions << new(klass)
       end
     end
+
+    def initialize(klass)
+      self.klass = klass
+    end
+
+    attr_reader :klass    
+
+    def create(data)
+      klass.new(data).save
+    end
+
+    def update(data)
+      model = find_model(data)
+      model.attributes = data
+      model.save
+    end
+
+    def destroy(data)
+      model = find_model(data)
+      model.destroy
+    end
+
+  private
+
+    def find_model(data)
+      klass.find_by_remote_id(data[:id])
+    end
+
+    attr_writer :klass
+
   end
 end
--- spec/model_subscriber_spec.rb
+++ spec/model_subscriber_spec.rb
@@ -7,7 +7,38 @@ describe MultipleMan::ModelSubscriber do
   describe "register" do 
     it "should add a subscriber" do
       MultipleMan::ModelSubscriber.register(MockClass)
-      MultipleMan::ModelSubscriber.subscriptions.should include MockClass
+      MultipleMan::ModelSubscriber.subscriptions[0].klass.should == MockClass
+    end
+  end
+
+  describe "create" do
+    it "should create a new model" do
+      mock_object = MockClass.new
+      MockClass.should_receive(:new).with({a: 1, b: 2}).and_return(mock_object)
+      mock_object.should_receive(:save)
+
+      MultipleMan::ModelSubscriber.new(MockClass).create({a: 1, b: 2})
+    end
+  end
+
+  describe "update" do
+    it "should find an existing model and update it" do
+      mock_object = MockClass.new
+      MockClass.should_receive(:find_by_remote_id).and_return(mock_object)
+      mock_object.should_receive(:attributes=)
+      mock_object.should_receive(:save)
+
+      MultipleMan::ModelSubscriber.new(MockClass).update({id: 5, a: 1, b: 2})
+    end
+  end
+
+  describe "destroy" do
+    it "should destroy the model" do
+      mock_object = MockClass.new
+      MockClass.should_receive(:find_by_remote_id).and_return(mock_object)
+      mock_object.should_receive(:destroy)
+
+      MultipleMan::ModelSubscriber.new(MockClass).destroy({id: 1})
     end
   end
 end
