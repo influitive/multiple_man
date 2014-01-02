7c8d06ad39c11343944c0cbaa7d184afe29ed34b
Ryan Brunner <ryan@influitive.com>
2014-01-02 12:50:14 -0500

    Don't log on init

--- lib/multiple_man/mixins/publisher.rb
+++ lib/multiple_man/mixins/publisher.rb
@@ -6,7 +6,6 @@ module MultipleMan
 
     module ClassMethods
       def publish(options = {})
-        MultipleMan.logger.info("Setting up publish for #{self.name}")
         after_commit ModelPublisher.new(:create, options), on: :create
         after_commit ModelPublisher.new(:update, options), on: :update
         after_commit ModelPublisher.new(:destroy, options), on: :destroy
--- lib/multiple_man/version.rb
+++ lib/multiple_man/version.rb
@@ -1,3 +1,3 @@
 module MultipleMan
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
