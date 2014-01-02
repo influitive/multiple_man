35722eb1dd1fa83ed24967d2b3644db631c1f9d2
Ryan Brunner <ryan@influitive.com>
2014-01-02 12:45:25 -0500

    Fixing logger on rails startup

--- lib/multiple_man/configuration.rb
+++ lib/multiple_man/configuration.rb
@@ -4,8 +4,11 @@ module MultipleMan
     def initialize
       self.topic_name = "multiple_man"
       self.app_name = Rails.application.class.parent.to_s if defined?(Rails)
-      self.logger = defined?(Rails) ? Rails.logger : Logger.new(STDOUT)
       self.enabled = true
+    end
+
+    def logger
+      @logger ||= defined?(Rails) ? Rails.logger : Logger.new(STDOUT)
     end
 
     attr_accessor :topic_name, :app_name, :connection, :logger, :enabled
