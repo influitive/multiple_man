4e04bb689a7f54bbc28f32adda83e7a23fc037a4
Ryan Brunner <ryan@influitive.com>
2014-04-09 11:34:25 -0400

    multiple_man_publisher was not working correctly with derived classes.

--- lib/multiple_man/mixins/publisher.rb
+++ lib/multiple_man/mixins/publisher.rb
@@ -5,6 +5,8 @@ module MultipleMan
       base.after_commit(on: :create) { |r| r.multiple_man_publish(:create) }
       base.after_commit(on: :update) { |r| r.multiple_man_publish(:update) }
       base.after_commit(on: :destroy) { |r| r.multiple_man_publish(:destroy) }
+
+      base.class_attribute :multiple_man_publisher
     end
 
     def multiple_man_publish(operation=:create)
@@ -16,8 +18,6 @@ module MultipleMan
       def multiple_man_publish(operation=:create)
         multiple_man_publisher.publish(self, operation) 
       end
-
-      attr_accessor :multiple_man_publisher
 
       def publish(options = {})
         self.multiple_man_publisher = ModelPublisher.new(options)
