4d5bbce596ff967fe2d8dc2fcaac21e3eb4c7e9d
Ryan Brunner <ryan@influitive.com>
2014-09-28 21:47:21 -0400

    Go back to safe individual connections

--- lib/multiple_man/connection.rb
+++ lib/multiple_man/connection.rb
@@ -4,37 +4,22 @@ require 'active_support/core_ext/module'
 
 module MultipleMan
   class Connection
-    @mutex = Mutex.new
-
-    def self.connection
-      @mutex.synchronize do 
-        # If the server has closed our connection, re-initialize
-        @connection = nil if @connection && @connection.closed?
-
-        @connection ||= begin
-          connection = Bunny.new(MultipleMan.configuration.connection)
-          MultipleMan.logger.debug "Connecting to #{MultipleMan.configuration.connection}"
-          connection.start
-          connection
-        end
-      end
-    end
 
     def self.connect
-      channel = nil
-      connection = self.connection
-      @mutex.synchronize do
-        channel = connection.create_channel
-      end
+      connection = Bunny.new(MultipleMan.configuration.connection)
+      MultipleMan.logger.debug "Connecting to #{MultipleMan.configuration.connection}"
+      connection.start
+      channel = connection.create_channel
       yield new(channel) if block_given?
     ensure
       channel.close if channel && channel.open?
+      connection.close if connection && connection.open?
     end
 
     attr_reader :topic
 
-    def initialize(channel)
-      self.channel = channel
+    def initialize
+      self.channel = self.class.connection.create_channel
       self.topic = channel.topic(topic_name)
     end
 
