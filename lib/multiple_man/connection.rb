55e3906d263bf2d32ec8409ea527e9f2196904d9
Ryan Brunner <ryan@influitive.com>
2015-05-14 16:28:41 -0400

    Revert "Possibly better connections?"
    This reverts commit b9f1b9aa7853fe8cef4b8a02d3cd6d88aa17d0db.

--- lib/multiple_man/connection.rb
+++ lib/multiple_man/connection.rb
@@ -1,18 +1,19 @@
 require 'bunny'
+require 'connection_pool'
 require 'active_support/core_ext/module'
 
 module MultipleMan
   class Connection
-    @mutex = Mutex.new
 
     def self.connect
+      connection = Bunny.new(MultipleMan.configuration.connection)
+      MultipleMan.logger.debug "Connecting to #{MultipleMan.configuration.connection}"
+      connection.start
       channel = connection.create_channel
       yield new(channel) if block_given?
-    rescue Bunny::Exception
-      reset!
-      retry
     ensure
       channel.close if channel && channel.open?
+      connection.close if connection && connection.open?
     end
 
     attr_reader :topic
@@ -20,24 +21,6 @@ module MultipleMan
     def initialize(channel)
       self.channel = channel
       self.topic = channel.topic(topic_name)
-    end
-
-    def self.connection
-      @mutex.synchronize do
-        @connection ||= begin
-          connection = Bunny.new(MultipleMan.configuration.connection, heartbeat: 5)
-          MultipleMan.logger.debug "Connecting to #{MultipleMan.configuration.connection}"
-          connection.start
-
-          connection
-        end
-      end
-    end
-
-    def self.reset!
-      @connection.close if @connection
-
-      @connection = nil
     end
 
     def topic_name
@@ -50,21 +33,6 @@ module MultipleMan
 
     attr_accessor :channel
     attr_writer :topic
+
   end
-end
-
-__END__
-
-# Possible usage
-
-Unicorn.after_fork do
-  MultipleMan::Connection.reset!
-end
-
-Sidekiq.configure_server do |config|
-  MultipleMan::Connection.reset!
-end
-
-PhusionPassenger.on_event(:starting_worker_process) do |forked|
-  MultipleMan::Connection.reset! if forked
 end
