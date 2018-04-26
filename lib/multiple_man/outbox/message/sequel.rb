14008f8445de250b560a6c67c2b0051deb805a8f
Bruno Da Silva <bruno.dasilva@influitive.com>
2018-04-26 18:20:13 +0000

    Add instrumentation to MM producer

--- lib/multiple_man/outbox/message/sequel.rb
+++ lib/multiple_man/outbox/message/sequel.rb
@@ -3,17 +3,29 @@ module MultipleMan
     module Message
       class Sequel < ::Sequel::Model(Outbox::DB.connection[:multiple_man_messages])
         def self.in_groups_and_delete(size = 100, &block)
-          messages = Outbox::DB.connection.fetch(grouped_by_limit_sql(size)).all
+          messages = fetch_messages_from_database(size)
+
           until messages.empty?
             yield(messages)
             where(id:
               messages.map { |h| h[:id] }
             ).delete
-            messages = Outbox::DB.connection.fetch(grouped_by_limit_sql(size)).all
+            messages = fetch_messages_from_database(size)
           end
         end
 
         private
+
+        def self.fetch_messages_from_database(size)
+          # This is just here for instrumentation, but hopefully the table stays small enough it will not impact performance much
+          table_count = Outbox::DB.connection.execute("SELECT COUNT(*) FROM multiple_man_messages;")
+
+          ActiveSupport::Notifications.instrument('multiple_man.producer.outbox_db_fetch', table_count: table_count) do |payload|
+            messages = Outbox::DB.connection.fetch(grouped_by_limit_sql(size)).all
+            payload[:message_count] = messages.size
+            messages
+          end
+        end
 
         def self.grouped_by_limit_sql(size)
           <<~SQL
